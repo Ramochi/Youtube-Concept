afc82b7b682bfba5ce1a0a74c2c4dd7780346c57
Ramochi <82056137+Ramochi@users.noreply.github.com>
2021-05-17 15:06:08 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,87 @@
+const myChannleSlider = new Swiper('.my-channel-slider',{
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6
+    },
+    1600: {
+       slidesPerView: 5
+    },
+    1300: {
+      slidesPerView: 4
+    },
+      1100: {
+      slidesPerView: 3
+    },
+    800: {
+      slidesPerView: 2
+    },
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.my-channel-button-next',
+    prevEl: '.my-channel-button-prev',
+  },
+});
+
+const recommendedSlider = new Swiper('.recommended-slider',{
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1600: {
+      slidesPerView: 3
+    },
+    1100: {
+      slidesPerView: 2
+    },
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.recommended-button-next',
+    prevEl: '.recommended-button-prev',
+  },
+});
+
+const recommendedChannleSlider = new Swiper('.recommended-channel-slider',{
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6
+    },
+    1600: {
+       slidesPerView: 5
+    },
+    1300: {
+      slidesPerView: 4
+    },
+      1100: {
+      slidesPerView: 3
+    },
+    800: {
+      slidesPerView: 2
+    },
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.recommended-channel-button-next',
+    prevEl: '.recommended-channel-button-prev',
+  },
+});
+const searchBtn = document.querySelector('.mobile-search');
+const mobileSearch = document.querySelector ('.input-group');
+searchBtn.addEventListener('click', () => {
+  mobileSearch.classList.toggle('is-open')
+});
+if (document.documentElement.scrollWidth <= 640) {
+  myChannleSlider.destroy();
+  recommendedSlider.destroy();
+  recommendedChannleSlider.destroy();
+}
